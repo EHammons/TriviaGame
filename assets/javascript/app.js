5c270dda5251c468d339db7b7ce24319c95ac183
Stardust <52759497+EHammons@users.noreply.github.com>
2019-09-14 01:16:29 -0500

    Working on displaying correct/incorrect GIFs

---
+++ assets/javascript/app.js
@@ -0,0 +1,4 @@
+var correctGif = $("<img src = https://media.giphy.com/media/YFis3URdQJ6qA/source.gif></img>");
+// console.log(correctGif);
+var image = document.getElementById("#image")
+correctGif.append(image);
