185e25e34ad92034f13ba954c0a815436e444801
Stardust <52759497+EHammons@users.noreply.github.com>
2019-09-14 20:05:45 -0500

    defining variables in js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,43 @@
-var correctGif = $("<img src = https://media.giphy.com/media/YFis3URdQJ6qA/source.gif></img>");
-// console.log(correctGif);
-var image = document.getElementById("#image")
-correctGif.append(image);
+var questions = {
+    qAndA: [{
+        q: "What is the name of Jasmine's tiger?", 
+        ans1: "Rajesh",
+        ans2: "Rajah",
+        ans3: "Abu",
+        ans4: "Sher Kahn"
+    }, {
+        q: "What are the name's of Cinderella's mice friends?", 
+        ans1: "Bianca & Bernard",
+        ans2: "Dumb & Dumber",
+        ans3: "Jaq & Gus",
+        ans4: "George & Lennie"
+    }, {
+        q: "How old is Snow White?",
+        ans1: "13",
+        ans2: "18",
+        ans3: "16", 
+        ans4: "14"
+    }, {
+        q: "How many 'thingamabobs' does Ariel have?", 
+        ans1: "Plenty",
+        ans2: "20",
+        ans3: "13",
+        ans4: "Too Many",
+    }, {
+        q: "Which princess has the least amount of screen time in her own movie?", 
+        ans1: "Aurora",
+        ans2: "Cinderella",
+        ans3: "Belle",
+        ans4: "Jasmine"
+    }, {
+        q: "Which character sings the song 'Beauty and the Beast'?", 
+        ans1: "Lumiere",
+        ans2: "Chip",
+        ans3: "Mrs. Potts",
+        ans4: "Cogsworth"
+    }]
+};
+
+var score = 0;
+var questionIndex = 0;
+
