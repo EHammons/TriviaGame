2a38fd0300bae9b0b6ba1f66f4eb6b3660b1a235
Stardust <52759497+EHammons@users.noreply.github.com>
2019-09-16 21:20:46 -0500

    One more try on images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,42 @@
 var questions = {
     qAndA: [{
         q: "What is the name of Jasmine's tiger?",
-        img: "<img src= '../assets/images/jasmine.jpg'>",
+        img: "<img src= 'TriviaGame/assets/images/jasmine.jpg'>",
         ans1: "Rajesh",
         ans2: "Rajah",
         ans3: "Abu",
         ans4: "Sher Kahn"
     }, {
         q: "What are the name's of Cinderella's mice friends?", 
-        img: "<img src='../assets/images/cinderella.jpg'>",
+        img: "<img src='TriviaGame/assets/images/cinderella.jpg'>",
         ans1: "Bianca & Bernard",
         ans2: "Dumb & Dumber",
         ans3: "Jaq & Gus",
         ans4: "George & Lennie"
     }, {
         q: "How old is Snow White?",
-        img: "<img src='../assets/images/snow-white.gif'>",
+        img: "<img src='TriviaGame/assets/images/snow-white.gif'>",
         ans1: "13",
         ans2: "18",
         ans3: "16", 
         ans4: "14"
     }, {
         q: "How many 'thingamabobs' does Ariel have?",
-        img: "<img src='../assets/images/ariel.jpg'>",
+        img: "<img src='TriviaGame/assets/images/ariel.jpg'>",
         ans1: "Plenty",
         ans2: "20",
         ans3: "13",
         ans4: "Too Many",
     }, {
         q: "Which princess has the least amount of screen time in her own movie?",
-        img: "<img src='../assets/images/princesses.jpg'>",
+        img: "<img src='TriviaGame/assets/images/princesses.jpg'>",
         ans1: "Aurora",
         ans2: "Cinderella",
         ans3: "Belle",
         ans4: "Jasmine"
     }, {
         q: "Which character sings the song 'Beauty and the Beast'?",
-        img: "<img src='../assets/images/belle.jpg'>",
+        img: "<img src='TriviaGame/assets/images/belle.jpg'>",
         ans1: "Lumiere",
         ans2: "Chip",
         ans3: "Mrs. Potts",
