5a8e02ef0ff798dd730d8a7e5ce69ccf80b383a0
Stardust <52759497+EHammons@users.noreply.github.com>
2019-09-16 20:58:01 -0500

    Image changes with question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,42 @@
 var questions = {
     qAndA: [{
         q: "What is the name of Jasmine's tiger?",
-        img: "/assets/images/jasmine.jpg",
+        img: "<img src=/assets/images/jasmine.jpg>",
         ans1: "Rajesh",
         ans2: "Rajah",
         ans3: "Abu",
         ans4: "Sher Kahn"
     }, {
         q: "What are the name's of Cinderella's mice friends?", 
-        img: "/assets/images/cinderella.jpg",
+        img: "<img src=/assets/images/cinderella.jpg>",
         ans1: "Bianca & Bernard",
         ans2: "Dumb & Dumber",
         ans3: "Jaq & Gus",
         ans4: "George & Lennie"
     }, {
         q: "How old is Snow White?",
-        img: "/assets/images/snow-white.gif",
+        img: "<img src=/assets/images/snow-white.gif>",
         ans1: "13",
         ans2: "18",
         ans3: "16", 
         ans4: "14"
     }, {
         q: "How many 'thingamabobs' does Ariel have?",
-        img: "/assets/images/ariel.jpg",
+        img: "<img src=/assets/images/ariel.jpg>",
         ans1: "Plenty",
         ans2: "20",
         ans3: "13",
         ans4: "Too Many",
     }, {
         q: "Which princess has the least amount of screen time in her own movie?",
-        img: "/assets/images/princesses.jpg",
+        img: "<img src=/assets/images/princesses.jpg>",
         ans1: "Aurora",
         ans2: "Cinderella",
         ans3: "Belle",
         ans4: "Jasmine"
     }, {
         q: "Which character sings the song 'Beauty and the Beast'?",
-        img: "/assets/images/belle.jpg",
+        img: "<img src=/assets/images/belle.jpg>",
         ans1: "Lumiere",
         ans2: "Chip",
         ans3: "Mrs. Potts",
@@ -77,17 +77,24 @@ function renderButtons() {
     $("#buttons").append(d);
 }
 
+function renderImage() {
+    $(".trivia-image").empty();
+    var image = $(questions.qAndA[questionIndex].img);
+    image.addClass("questionImage");
+    $(".trivia-image").append(image);
+}
+
 function renderQuestion() {
     if (questionIndex <+ (questions.qAndA.length - 1)) {
         $("#question").text(questions.qAndA[questionIndex].q);
         renderButtons();
-        var image = $("<img src =" + questions.qAndA[questionIndex].img + "></img>");
-        $("#trivia-image").text(image);
+        renderImage();
         timer();
     } else {
         $("#time").text("Game Over!");
         $("#question").text("Final Score: " + score + " out of " + questions.qAndA.length);
         $("#buttons").empty();
+        $(".trivia-image").empty();
     }
 }
 
@@ -116,7 +123,6 @@ function stop() {
     renderQuestion();
 }
 
-renderButtons();
 renderQuestion();
 updateScore();
 
